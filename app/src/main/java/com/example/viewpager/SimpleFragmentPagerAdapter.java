5fe12807172cb7e05d295c95c9435dc0e9d6de82
Raul Serrano Mena <raulserranomena@gmail.com>
2020-05-04 12:18:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.viewpager;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new com.example.viewpager.MondayFragment();
+        } else if (position == 1){
+            return new com.example.viewpager.TuesdayFragment();
+        } else {
+            return new com.example.viewpager.WednesdayFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
